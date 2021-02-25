a62af065169bdfb74993dc4bc539983032ab1bee
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-02-25 15:52:29 +0200

    init

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,18 @@ func routes(_ app: Application) throws {
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
+    
+    app.get("hello", "vapor") { req -> String in
+        return "Hello, Vapor!"
+    }
+    
+    app.post("user") { req -> String in
+        let greeting = try req.content.decode(UserInfo.self)
+        print("Hello, \(greeting.name)")
+        return "Hello, \(greeting.name)"
+    }
+}
+
+struct UserInfo: Content {
+    let name: String
 }
